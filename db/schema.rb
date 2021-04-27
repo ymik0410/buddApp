fa9aa9089376c444b4c81b37f5bd57b69d95618a
Yulia Mikhaylova ymik0410@gmail.com <ymik0410@gmail.com>
2021-04-27 10:19:32 +0200

    I changed the column name for goal_description

--- db/schema.rb
+++ db/schema.rb
@@ -33,7 +33,7 @@ ActiveRecord::Schema.define(version: 2021_04_26_153010) do
     t.string "first_name"
     t.string "last_name"
     t.string "introduction"
-    t.string "goalDescription"
+    t.string "goal_description"
     t.string "photo"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
