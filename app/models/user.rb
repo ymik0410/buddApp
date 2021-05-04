61cba97d3179e40ef362eec80b4d95607c8d0dbd
Yulia Mikhaylova ymik0410@gmail.com <ymik0410@gmail.com>
2021-05-04 23:27:37 +0200

    Edited sign-up and edit profile pages

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   has_many :histories, dependent: :destroy
   has_many :goals, through: :histories
   has_one_attached :image
-  GENDERS = ["Female", "Male", "Rather not say"]
+  validates :username, presence: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
