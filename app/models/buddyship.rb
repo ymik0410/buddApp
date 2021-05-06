c0e2398582a0eac457c757bf4a5580c85ad0ef9b
Santa Cadusch <santacadusch@icloud.com>
2021-05-06 14:05:04 +0200

    done chat and removing buddy

--- app/models/buddyship.rb
+++ app/models/buddyship.rb
@@ -1,5 +1,5 @@
 class Buddyship < ApplicationRecord
   belongs_to :user1, class_name: "User", foreign_key: "user1_id"
   belongs_to :user2, class_name: "User", foreign_key: "user2_id"
-  has_many :messages
+  has_many :messages, dependent: :destroy
 end
